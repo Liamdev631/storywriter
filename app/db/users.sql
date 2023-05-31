772cedc462bdfb553e71610e13dc69925d8359d6
Liamdev631 <liamdev631@gmail.com>
2023-05-31 12:51:51 -0400

    Removed Flask in favor of Streamlit

--- app/db/users.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE [dbo].[users]
-(
-  [Id] INT NOT NULL PRIMARY KEY
-)
--- generators/generator.py
+++
@@ -1,7 +0,0 @@
-
-from abc import ABC, abstractstaticmethod
-
-class AbstractPromptGen(ABC):
-    @abstractstaticmethod
-    def generate() -> PromptTemplate:
-        return "@abstractstaticmethod called!"
