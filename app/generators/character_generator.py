0ac50a8cd9d12d7265accc56d0db6b2e84121a60
Liamdev631 <liamdev631@gmail.com>
2023-06-06 14:12:55 -0400

    Fixed broken llm result parsing

--- app/generators/character_generator.py
+++ app/generators/character_generator.py
@@ -71,6 +71,6 @@ class CharacterGenerator(Generator):
         (Neuroticism: {neuroticism}%).
         """
         prompt: str = PromptTemplate.from_template(template=template).format(**params)
-        return llm(messages=[prompt])
+        return llm(messages=[prompt]).content
 
 
